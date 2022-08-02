d338bfc58239eedadd238df6da5be8a44d9493ab
Kirill Priyatnov <kirill.priyatnov@gmail.com>
2022-08-02 09:03:15 +0300

    add controller on /commitId integrate git status with actuator

--- src/main/java/ru/skillfactory/demoWebApp/DemoWebAppApplication.java
+++ src/main/java/ru/skillfactory/demoWebApp/DemoWebAppApplication.java
@@ -2,12 +2,25 @@ package ru.skillfactory.demoWebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class DemoWebAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoWebAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebAppApplication.class, args);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propsConfig
+                = new PropertySourcesPlaceholderConfigurer();
+        propsConfig.setLocation(new ClassPathResource("git.properties"));
+        propsConfig.setIgnoreResourceNotFound(true);
+        propsConfig.setIgnoreUnresolvablePlaceholders(true);
+        return propsConfig;
+    }
 
 }
